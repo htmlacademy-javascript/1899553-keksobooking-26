e391cb9a5026419661cbb8317a2912ee3e27bb92
Aleksandra Kuznetsova <alexsandrakuznetsova0806@gmail.com>
2022-06-22 09:35:15 +0300

    Внесла правки

--- js/main.js
+++ js/main.js
@@ -29,27 +29,33 @@ const getRandomLength = function (array) {
   return array.slice(0, lengthArray);
 };
 
-const createObject = function () {
-  const RANDOM_NUMBER = getRandomInRange(1, 10);
-  const ZERO = '0';
-  const AUTHOR = {
-    avatar: `img / avatars / user${RANDOM_NUMBER === 10 ? RANDOM_NUMBER : ZERO + RANDOM_NUMBER}.png`
-  }
-  const TITLE = ['Рядом с метро', 'В центре города', 'Тихий район', 'Рядом с парком'];
-  const LOCATION = {
-    lat: getRandomFloat(35.65, 35.7, 5),
-    lng: getRandomFloat(139.7, 139.8, 5)
+const randomNumber = () => {
+  for (let i = 1; i <= 10; i++) {
+    if (i >= 10) {
+      return i;
+    } else { return '0' + i; }
   };
-  const ADDRESS = LOCATION;
-  const PRICE = getRandomInRange(1500, 5999);
-  const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const ROOMS = getRandomInRange(1, 6);
-  const GUESTS = getRandomInRange(1, 12);
-  const CHECKIN = ['12:00', '13:00', '14:00'];
-  const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const DESCRIPTION = ['отличный вид', 'светлая квартира'];
-  const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+}
 
+const TITLE = ['Рядом с метро', 'В центре города', 'Тихий район', 'Рядом с парком'];
+const LOCATION = {
+  lat: getRandomFloat(35.65, 35.7, 5),
+  lng: getRandomFloat(139.7, 139.8, 5)
+};
+const PRICE = getRandomInRange(1500, 5999);
+const TYPE = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const ROOMS = getRandomInRange(1, 6);
+const GUESTS = getRandomInRange(1, 12);
+const CHECKIN = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTION = ['отличный вид', 'светлая квартира'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const createObject = function () {
+  const RANDOM_NUMBER = randomNumber();
+  const AUTHOR = {
+    avatar: `img / avatars / user${RANDOM_NUMBER}.png`
+  };
 
   return {
     author: AUTHOR,
@@ -72,7 +78,4 @@ const createObject = function () {
 
 const NUMBER_OF_OBJECTS = 10;
 
-const createObjects = Array.from({length:NUMBER_OF_OBJECTS}, createObject);
-const objectsFunc = () => createObjects;
-
-console.log(objectsFunc());
+const createObjects = Array.from({ length: NUMBER_OF_OBJECTS }, createObject);
